0560276ac4977e59e1b4ad3cda893ef90dd1b199
giova <giovanaftiburtino@gmail.com>
2022-09-08 15:34:48 -0300

    Exercício 1,2 e 3 realizados

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -3,12 +3,13 @@ const num = prompt("Digite um número aqui")
 //Utilizando ifs aninhados
 
 if (num % 2 === 0) {
-    if (num % 3 === 0) {
+    console.log("O número é divisível por 2")
+} else {
+    console.log("O número não é divisível por 2")
+} if (num % 3 === 0) {
     console.log("O número é divisível por 3")
     } else {
     console.log("O número não é divisível por 3")
-}} else {
-    console.log("O número não é divisível por 2")
 }
 
 
